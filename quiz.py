b4b5a93f2be7dd170d28c4164a47167c5cd045c7
Vinz-A <vinz_abu@student.asdah.school.nz>
2022-05-19 13:09:36 +1200

    Fixed everything (Basically done)

--- quiz.py
+++ quiz.py
@@ -30,166 +30,165 @@ while(continuing):
     print("Invaid answer, try again")
 
 continuing = True
-while(continuing):
-    
+while(continuing):  
  q2 = input("Question 2: What is the name of the main antagonist? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Akaza\nb) Nezuko\nc) Muzan\nd) Michael Jackson\n>>") 
 
-if  q2 == "c":
+ if q2 == "c":
     score += 1
     print("Correct\nTotal Score: " + str(score))
     continuing = False
-elif q2 == "a" or q2 == "b" or q2 == "d":
+ elif q2 == "a" or q2 == "b" or q2 == "d":
     print("Incorrect")
     continuing = False
-else:
+ else:
     print("Invaid answer, try again")
 
-q3 = input("Question 3: What is the name of the yellow-haired boy? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Tanjiro\nb) Nezuko\nc) Inosuke\nd) Zenistu\n>>")  
 continuing = True
 while(continuing):
+ q3 = input("Question 3: What is the name of the yellow-haired boy? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Tanjiro\nb) Nezuko\nc) Inosuke\nd) Zenistu\n>>")  
 
-if  q3 == "d":
+ if  q3 == "d":
     score += 1
     print("Correct\nTotal Score: " + str(score))
     continuing = False
-elif q3 == "a" or q3 == "b" or q3 == "c":
+ elif q3 == "a" or q3 == "b" or q3 == "c":
     print("Incorrect")
     continuing = False
-else:
+ else:
     print("Invaid answer, try again")
 
-q4 = input("Question 4: What breathing form does Rengoku use? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Water\nb) Earth\nc) Fire\nd) Air\n>>")
 continuing = True
 while(continuing):
+ q4 = input("Question 4: What breathing form does Rengoku use? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Water\nb) Earth\nc) Fire\nd) Air\n>>")
 
-if  q4 == "c":
+ if  q4 == "c":
     score += 1
     print("Correct\nTotal Score: " + str(score))
     continuing = False
-elif q4 == "a" or q4 == "b" or q4 == "d":
+ elif q4 == "a" or q4 == "b" or q4 == "d":
     print("Incorrect")
     continuing = False
-else:
+ else:
     print("Invaid answer, try again")
 
-q5 = input("Question 5: What do demons have? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Regeneration\nb) Fire Resistance\nc) Love\nd) Nothing\n>>")
 continuing = True
 while(continuing):
+ q5 = input("Question 5: What do demons have? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Regeneration\nb) Fire Resistance\nc) Love\nd) Nothing\n>>")
 
-if  q5 == "a":
+ if  q5 == "a":
     score += 1
     print("Correct\nTotal Score: " + str(score))
     continuing = False
-elif q5 == "b" or q5 =="c" or q5 == "d":
+ elif q5 == "b" or q5 =="c" or q5 == "d":
     print("Incorrect")
     continuing = False
-else:
+ else:
     print("Invaid answer, try again")
 
-q6 = input ("Question 6: Who is the most powerful Demon slayer? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Tanjiro\nb) Muzan\nc) Gyomei\nd) Yorrichi\n>>")  
 continuing = True
 while(continuing):
+ q6 = input ("Question 6: Who is the most powerful Demon slayer? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Tanjiro\nb) Muzan\nc) Gyomei\nd) Yorrichi\n>>")  
 
-if  q6 == "d":
+ if  q6 == "d":
     score += 1
     print("Correct\nTotal Score: " + str(score))
     continuing = False
-elif q6 == "a" or q6 == "b" or q6 == "c":
+ elif q6 == "a" or q6 == "b" or q6 == "c":
     print("Incorrect")
     continuing = False
-else:
+ else:
     print("Invaid answer, try again")
 
-q7 = input("Question 7: Which is the most powerful Hashira? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Shinobu\nb) Gyomei\nc) Rengoku\nd) Giyu\n>>")
 continuing = True
 while(continuing):
+ q7 = input("Question 7: Which is the most powerful Hashira? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Shinobu\nb) Gyomei\nc) Rengoku\nd) Giyu\n>>")
 
-if  q7 == "b":
+ if  q7 == "b":
     score += 1
     print("Correct\nTotal Score: " + str(score))
     continuing = False
-elif q7 == "a" or q7 == "c" or q7 == "d":
+ elif q7 == "a" or q7 == "c" or q7 == "d":
     print("Incorrect")
     continuing = False
-else:
+ else:
     print("Invaid answer, try again")
 
-q8 = input("Question 8: What year was the Demon slayer movie released in? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) 2018\nb) 2019\nc) 2020\nd) 2021\n>>")  
 continuing = True
 while(continuing):
+ q8 = input("Question 8: What year was the Demon slayer movie released in? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) 2018\nb) 2019\nc) 2020\nd) 2021\n>>")  
 
-if q8 == "c":
+ if q8 == "c":
     score += 1
     print("Correct\nTotal Score: " + str(score))
     continuing = False
-elif q8 == "a" or q8 == "b" or q8 == "d":
+ elif q8 == "a" or q8 == "b" or q8 == "d":
     print("Incorrect")
     continuing = False
-else:
+ else:
     print("Invaid answer, try again")
 
-q9 = input("Question 9: Which of these is the most powerful demon? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Kokushibo\nb) Akaza\nc) Douma\nd) Daki\n>>")
 continuing = True
 while(continuing):
+ q9 = input("Question 9: Which of these is the most powerful demon? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Kokushibo\nb) Akaza\nc) Douma\nd) Daki\n>>")
 
-if  q9 == "a":
+ if  q9 == "a":
     score += 1
     print("Correct\nTotal Score: " + str(score))
     continuing = False
-elif q9 == "b" or q9 == "c" or q9 == "d":
+ elif q9 == "b" or q9 == "c" or q9 == "d":
     print("Incorrect")
     continuing = False
-else:
+ else:
     print("Invaid answer, try again")
 
-q10 = input("Question 10: What are demons scared of? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Homework\nb) Grass\nc) Wifi\nd) Sunlight\n>>")
 continuing = True
 while(continuing):
+ q10 = input("Question 10: What are demons scared of? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Homework\nb) Grass\nc) Wifi\nd) Sunlight\n>>")
 
-if  q10 == "d":
+ if  q10 == "d":
     score += 1
     print("Correct\nTotal Score: " + str(score))
     continuing = False
-elif q10 == "a" or q10 == "b" or q10 == "c":
+ elif q10 == "a" or q10 == "b" or q10 == "c":
     print("Incorrect")
     continuing = False
-else:
+ else:
     print("Invaid answer, try again")
 
-q11 = input("Question 11: Do demons talk? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Yes\nb) No\nc) Spanish\nd) Ancient Demonic Language\n>>")  
 continuing = True
 while(continuing):
+ q11 = input("Question 11: Do demons talk? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) Yes\nb) No\nc) Spanish\nd) Ancient Demonic Language\n>>")  
 
-if  q11 == "a" :
+ if  q11 == "a" :
     score += 1
     print("Correct\nTotal Score: " + str(score))
     continuing = False
-elif q11 == "b" or q11 == "c" or q11 =="d":
+ elif q11 == "b" or q11 == "c" or q11 =="d":
     print("Incorrect")
     continuing = False
-else:
+ else:
     print("Invaid answer, try again")
 
-q12 = input("Final question: How many Wives does Tengan Uzui have? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) 1\nb) 2\nc) 3\nd) 4\n>>") 
 continuing = True
 while(continuing):
+ q12 = input("Final question: How many Wives does Tengan Uzui have? (By the way just put ONLY the letter in lowercase. E.g 'a')\na) 1\nb) 2\nc) 3\nd) 4\n>>") 
 
-if q12 == "a":
+ if q12 == "a":
     score += 1
     print("Correct\nTotal Score: " + str(score))
     continuing = False
-elif q12 == "b" or q12 == "c" or q12 == "d":
+ elif q12 == "b" or q12 == "c" or q12 == "d":
     print("Incorrect")
     continuing = False
-else:
+ else:
     print("Invaid answer, try again")
 
 # Finalise score
 if str(score) == "12":
-    print("Total: {}/12, You are Amazing, You really know your things".format(str(score)))
+    print("{}/12 Correct, You are Amazing, You really know your things".format(str(score)))
 
 elif score >= 7 and score < 12:
-    print("Total: {}/12, You did well, Hopefully you get a perfect score next time".format(str(score)))
+    print("{}/12 Correct, You did well, Hopefully you get a perfect score next time".format(str(score)))
 
 else:
-    print("Total: {}/12, Good Try, Hopefully you do better next time".format(str(score)))
+    print("{}/12 Correct, Good Try, Hopefully you do better next time".format(str(score)))
